6d3611f489de6c28e41efa58a9c59f2397a224e7
sharad shivaji sable <sssable1999@gmail.com>
2025-10-19 16:37:11 +0530

    initial commit

---
+++ src/main/java/org/school/equipment/lending/SchoolEquipmentLendingApplication/SchoolEquipmentLendingApplication.java
@@ -0,0 +1,13 @@
+package org.school.equipment.lending.SchoolEquipmentLendingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolEquipmentLendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolEquipmentLendingApplication.class, args);
+	}
+
+}
